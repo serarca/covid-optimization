9f68d5a3844cb9ce7e8e41fe8e0d83eef3a1bf35
Sergio A Camelo Gomez <sergioacamelogomez@DNa1c2598.SUNet>
2020-04-30 13:32:53 -0700

    more functions

--- group.py
+++ group.py
@@ -5,31 +5,53 @@ class SEIR_group:
 	def __init__(self, parameters):
 		# Group name
 		self.name = parameters['name']
+		self.parameters = parameters['parameters']
 
 		# Time step
-		self.t = 0.0
+		self.t = 0
 
 		# Susceptible 
-		self.S = parameters['initial-conditions']['S']
+		self.S = [float(parameters['initial-conditions']['S'])]
 		# Exposed (unquarentined)
-		self.E = parameters['initial-conditions']['E']
+		self.E = [float(parameters['initial-conditions']['E'])]
 		# Infected (unquarentined)
-		self.I = parameters['initial-conditions']['I']
+		self.I = [float(parameters['initial-conditions']['I'])]
 		# Unquarentined patients 
-		self.N = self.S + self.E + self.I
-
+		self.N = [self.S + self.E + self.I]
 		# Recovered (unquarentined)
-		self.R = parameters['initial-conditions']['R']
+		self.R = [float(parameters['initial-conditions']['R'])]
 
+	# Attach other groups to make it easier to find variables of other groups
 	def attach_other_groups(self,all_groups):
-		self.other_groups = all_groups
-		
+		self.all_groups = all_groups
 
+	# Advances one time step, given the m_tests and a_tests variable
 	def take_time_step(self, m_tests, a_tests):
-		new_N = self.updated_N(m_tests, a_tests)
+		self.update_N(m_tests, a_tests)
 
-		self.N =  new_N
+	# Updates N
+	def update_N(self, m_tests, a_tests):
+		delta_N = -m_tests*self.I[self.t]/self.N[self.t] - a_tests*self.R[self.t]/self.N[self.t]
+		self.N += [self.N[self.t]+delta_N*dt]
 
-	def updated_N(self, m_tests, a_tests):
-		new_N = self.N - m_tests*self.I/self.N - a_tests*self.R/self.N
+	# Updates S
+	def update_S(self, m_tests, a_tests):
+		infections = 0
+		for name,group in self.all_groups.iteritems():
+			infections += self.parameters['contacts'][name]*group.I[self.t]/group.N[self.t]
+
+		delta_S = -self.parameters['beta']*self.S[self.t]*infections
+		self.S += [self.S[self.t]+delta_S*dt]
+
+	def update_E(self, m_tests, a_tests):
+		infections = 0
+		for name,group in self.all_groups.iteritems():
+			infections += self.parameters['contacts'][name]*group.I[self.t]/group.N[self.t]
+		delta_E = self.parameters['beta']*self.S[self.t]*infections - self.parameters['sigma']*self.E[self.t]
+		self.E += [self.E[self.t]+delta_E*dt]
+
+	
+
+
+
 
