7680eb17991e8a1e3874545bc30e5be5b297bea2
Sergio A Camelo Gomez <sergioacamelogomez@DNa1c2598.SUNet>
2020-04-30 15:12:06 -0700

    more functions

--- group.py
+++ group.py
@@ -12,14 +12,32 @@ class SEIR_group:
 
 		# Susceptible 
 		self.S = [float(parameters['initial-conditions']['S'])]
-		# Exposed (unquarentined)
+		# Exposed (unquarantined)
 		self.E = [float(parameters['initial-conditions']['E'])]
-		# Infected (unquarentined)
+		# Infected (unquarantined)
 		self.I = [float(parameters['initial-conditions']['I'])]
-		# Unquarentined patients 
+		# Unquarantined patients 
 		self.N = [self.S + self.E + self.I]
-		# Recovered (unquarentined)
+		# Recovered (unquarantined)
 		self.R = [float(parameters['initial-conditions']['R'])]
+
+		# Infected quarantined with different degrees of severity
+		self.I_a = [float(parameters['initial-conditions']['I_a'])]
+		self.I_ps = [float(parameters['initial-conditions']['I_ps'])]
+		self.I_ms = [float(parameters['initial-conditions']['I_ms'])]
+		self.I_ss = [float(parameters['initial-conditions']['I_ss'])]
+
+		# Recovered quanrantined
+		self.Rq = [float(parameters['initial-conditions']['Rq'])]
+
+		# In hospital bed
+		self.H = [float(parameters['initial-conditions']['H'])]
+		# In ICU
+		self.ICU = [float(parameters['initial-conditions']['ICU'])]
+		# Dead
+		self.D = [float(parameters['initial-conditions']['D'])]
+
+
 
 	# Attach other groups to make it easier to find variables of other groups
 	def attach_other_groups(self,all_groups):
@@ -50,7 +68,62 @@ class SEIR_group:
 		delta_E = self.parameters['beta']*self.S[self.t]*infections - self.parameters['sigma']*self.E[self.t]
 		self.E += [self.E[self.t]+delta_E*dt]
 
-	
+	def update_I(self, m_tests, a_tests):
+		delta_I = self.parameters['sigma']*self.E[self.t] - self.parameters['mu']*self.I[self.t] - m_tests*self.I[self.t]/self.N[self.t]
+		self.I += [self.I[self.t]+delta_I*dt]
+
+	def update_R(self, m_tests, a_tests):
+		delta_R = self.parameters['mu']*(1-self.parameters["p_H"]+self.parameters["p_ICU"])*self.I[self.t] - a_tests*self.R[self.t]/self.N[self.t]
+		self.R += [self.R[self.t]+delta_R*dt]
+
+	def update_Ia(self, m_tests, a_tests):
+		delta_Ia = self.parameters['p_Ia']*m_tests*self.I[self.t]/self.N[self.t] - parameters['mu']*self.Ia[self.t]
+		self.Ia += [self.Ia[self.t]+delta_Ia*dt]
+
+	def update_Ips(self, m_tests, a_tests):
+		delta_Ips = self.parameters['p_Ips']*m_tests*self.I[self.t]/self.N[self.t] - parameters['mu']*self.Ips[self.t]
+		self.Ips += [self.Ips[self.t]+delta_Ips*dt]
+
+	def update_Ims(self, m_tests, a_tests):
+		delta_Ims = self.parameters['p_Ims']*m_tests*self.I[self.t]/self.N[self.t] - parameters['mu']*self.Ims[self.t]
+		self.Ims += [self.Ims[self.t]+delta_Ims*dt]
+
+	def update_Iss(self, m_tests, a_tests):
+		delta_Iss = self.parameters['p_Iss']*m_tests*self.I[self.t]/self.N[self.t] - parameters['mu']*self.Iss[self.t]
+		self.Iss += [self.Iss[self.t]+delta_Iss*dt]
+
+	def update_Rq(self, m_tests, a_tests):
+		delta_Rq = (
+			self.parameters['mu']*(self.Ia[self.t]+self.Ips[self.t]+self.Ims[self.t]) + 
+			self.parameters['lambda_H']*self.H[self.t] +
+			self.parameters['lambda_ICU']*self.ICU[self.t] +
+			a_tests*self.R[self.t]/self.N[self.t]
+		)
+		self.Rq += [self.Rq[self.t]+delta_Rq*dt]
+
+	def update_H(self, m_tests, a_tests):
+		# For each group, calculate the entering amount
+		entering = {}
+		summ_entering = 0
+		for n,g in self.all_groups:
+			entering[g] = parameters['mu']*parameters['p_H']*(g.I[self.t]+g.I_ss[self.t])
+			summ_entering += entering[g]
+		# Calculate number of beds
+		beds = self.parameters['C_H']
+		for n,g in self.all_groups:
+			beds-=g.H[self.t]
+
+		delta_H = entering[self.name]*(1-(summ_entering-beds)/summ_entering)
+
+
+
+
+
+
+
+
+
+
 
 
 
