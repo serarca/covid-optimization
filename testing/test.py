06424e6188f85d5ccafd0786dacccc3224e1c2e4
Sergio Camelo <sergioacamelogomez@DN51sck0.SUNet>
2020-05-05 09:01:21 -0700

    added tests

--- testing/test.py
+++ testing/test.py
@@ -25,7 +25,7 @@ with open(args.data) as file:
 
 # Set up parameters of simulation
 dt = 0.1
-total_time = 80
+total_time = 100
 
 time_periods = int(round(total_time/dt))
 
@@ -48,14 +48,14 @@ time_axis = [i*dt for i in range(time_periods+1)]
 
 
 plt.figure(1)
-plt.subplot(3,2,1)
+plt.subplot(5,2,1)
 plt.plot(time_axis, dynModel.groups['young'].S, label="Susceptible")
 plt.plot(time_axis, dynModel.groups['young'].E, label="Exposed")
 plt.plot(time_axis, dynModel.groups['young'].I, label="Infected")
 plt.plot(time_axis, dynModel.groups['young'].R, label="Recovered")
 plt.title('Young')
 
-plt.subplot(3,2,2)
+plt.subplot(5,2,2)
 plt.plot(time_axis, dynModel.groups['old'].S, label="Susceptible")
 plt.plot(time_axis, dynModel.groups['old'].E, label="Exposed")
 plt.plot(time_axis, dynModel.groups['old'].I, label="Infected")
@@ -63,16 +63,23 @@ plt.plot(time_axis, dynModel.groups['old'].R, label="Recovered")
 plt.legend(loc='upper right')
 plt.title('Old')
 
-plt.subplot(3,2,3)
+plt.subplot(5,2,3)
 plt.plot(time_axis, dynModel.groups['young'].Rq, label="Recovered Q")
+plt.xlabel('Time')
+
+plt.subplot(5,2,4)
+plt.plot(time_axis, dynModel.groups['old'].Rq, label="Recovered Q")
+plt.legend(loc='upper right')
+plt.xlabel('Time')
+
+plt.subplot(5,2,5)
 plt.plot(time_axis, dynModel.groups['young'].Ia, label="Infected A-Q")
 plt.plot(time_axis, dynModel.groups['young'].Ips, label="Infected PS-Q")
 plt.plot(time_axis, dynModel.groups['young'].Ims, label="Infected MS-Q")
 plt.plot(time_axis, dynModel.groups['young'].Iss, label="Infected SS-Q")
 plt.xlabel('Time')
 
-plt.subplot(3,2,4)
-plt.plot(time_axis, dynModel.groups['old'].Rq, label="Recovered Q")
+plt.subplot(5,2,6)
 plt.plot(time_axis, dynModel.groups['old'].Ia, label="Infected A-Q")
 plt.plot(time_axis, dynModel.groups['old'].Ips, label="Infected PS-Q")
 plt.plot(time_axis, dynModel.groups['old'].Ims, label="Infected MS-Q")
@@ -81,22 +88,33 @@ plt.legend(loc='upper right')
 plt.xlabel('Time')
 
 
-plt.subplot(3,2,5)
+plt.subplot(5,2,7)
 plt.plot(time_axis, dynModel.groups['young'].H, label="Hospital Bed")
 plt.plot(time_axis, dynModel.groups['young'].ICU, label="ICU")
 plt.plot(time_axis, dynModel.groups['young'].D, label="Dead")
 plt.xlabel('Time')
 
 
-plt.subplot(3,2,6)
+plt.subplot(5,2,8)
 plt.plot(time_axis, dynModel.groups['old'].H, label="Hospital Bed")
 plt.plot(time_axis, dynModel.groups['old'].ICU, label="ICU")
 plt.plot(time_axis, dynModel.groups['old'].D, label="Dead")
 plt.legend(loc='upper right')
 plt.xlabel('Time')
 
+plt.subplot(5,1,5)
+plt.plot(time_axis, [dynModel.groups['old'].H[i] + dynModel.groups['young'].H[i] for i in range(len(time_axis))], label="Total Hospital Beds")
+plt.plot(time_axis, [dynModel.groups['old'].ICU[i] + dynModel.groups['young'].ICU[i] for i in range(len(time_axis))], label="Total ICUs")
+plt.axhline(y=parameters['global-parameters']['C_H'], color='r', linestyle='dashed', label= "Hospital Capacity")
+plt.axhline(y=parameters['global-parameters']['C_ICU'], color='g', linestyle='dashed', label= "ICU Capacity")
+plt.legend(loc='upper right')
+plt.xlabel('Time')
+
+
 
 figure = plt.gcf() # get current figure
 figure.set_size_inches(12, 12)
 plt.savefig(args.data.split(".")[0]+".png", dpi = 100)
+
+# print([dynModel.groups['old'].ICU[i] + dynModel.groups['young'].ICU[i] for i in range(len(time_axis))])
 
