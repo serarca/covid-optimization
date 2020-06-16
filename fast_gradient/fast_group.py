3d328fd53e313336dc14e100f4ee2155d19d5701
serarca <serarcago@gmail.com>
2020-06-15 18:50:13 -0700

    added fast calculations of dynamical model

---
+++ fast_gradient/fast_group.py
@@ -0,0 +1,292 @@
+from collections import defaultdict
+from bound import Bounds
+import numpy as np
+import pandas as pd
+import math
+
+age_groups = ['age_group_0_9', 'age_group_10_19', 'age_group_20_29','age_group_30_39', 'age_group_40_49', 'age_group_50_59', 'age_group_60_69', 'age_group_70_79', 'age_group_80_plus']
+cont = [ 'S', 'E', 'I', 'R', 'N', 'Ia', 'Ips', \
+       'Ims', 'Iss', 'Rq', 'H', 'ICU', 'D' ]
+activities = ['home','leisure','other','school','transport','work']
+
+
+class FastDynamicalModel:
+	def __init__(self, parameters, initialization, dt, mixing_method):
+		self.parameters = parameters
+		self.dt = dt
+		self.initialization = initialization
+		self.mixing_method = mixing_method
+
+		# Create groups from parameters
+		self.groups = {}
+		for n in parameters['seir-groups']:
+			self.groups[n] = SEIR_group(parameters['seir-groups'][n], initialization[n], self.dt, self.mixing_method, self)
+
+
+		# Fix number of beds and icus
+		self.beds = self.parameters['global-parameters']['C_H']
+		self.icus = self.parameters['global-parameters']['C_ICU']
+
+		# Initialize total population
+		self.total_population = sum([sum([initialization[group][cat] for cat in initialization[group].keys()]) for group in initialization.keys()])
+
+		# Initialize M matrix
+		self.initialize_M()
+
+		# Initialize arrays
+		self.contact_matrix = np.zeros((len(age_groups),len(age_groups)))
+
+		# Initialize parameters vectors
+		self.p_H = self.params_vector("p_H")
+		self.p_ICU = self.params_vector("p_ICU")
+		self.mu = self.params_vector("mu")
+		self.sigma = self.params_vector("sigma")
+		self.beta = self.params_vector("beta")
+		self.p_Ia = self.params_vector("p_Ia")
+		self.p_Ips = self.params_vector("p_Ips")
+		self.p_Ims = self.params_vector("p_Ims")
+		self.p_Iss = self.params_vector("p_Iss")
+		self.lambda_H_R = self.params_vector("lambda_H_R")
+		self.lambda_ICU_R = self.params_vector("lambda_ICU_R")
+		self.lambda_H_D = self.params_vector("lambda_H_D")
+		self.lambda_ICU_D = self.params_vector("lambda_ICU_D")
+
+	def params_vector(self, param):
+		v = np.zeros(len(age_groups))
+		for i in range(len(age_groups)):
+			v[i] = self.groups[age_groups[i]].parameters[param]
+		return v
+
+
+	def initialize_M(self):
+		self.M = np.zeros((len(age_groups),len(age_groups), len(activities)))
+		for g1 in range(len(age_groups)):
+			for g2 in range(len(age_groups)):
+				for act in range(len(activities)):
+					self.M[g1,g2,act] = self.groups[age_groups[g1]].contacts[activities[act]][age_groups[g2]]
+
+
+	def take_time_step(self, state, m_tests, a_tests, alphas, B_H = False, B_ICU = False):
+
+		# Store variables
+		self.state = state
+		self.m_tests = m_tests
+		self.a_tests = a_tests
+		self.alphas = alphas
+		self.B_H = B_H
+		self.B_ICU = B_ICU
+
+		# Create new state
+		self.new_state = np.zeros((len(age_groups),len(cont)))
+
+		# Update contact matric
+		self.update_contact_matrix()
+
+		# Update total contacts
+		pop = self.state[:,cont.index("N")] + self.state[:,cont.index("Rq")]
+		pop[pop==0.0] = 1e-6
+		self.total_contacts = self.state[:,cont.index("S")]*np.matmul(
+			self.contact_matrix,
+			self.state[:,cont.index("I")]/pop
+		)
+
+		# Construct flow variables
+		self.flow_H = self.get_flow_H()
+		self.flow_ICU = self.get_flow_ICU()
+
+		# # Construct prorrated bouncing
+		if ((B_H is False) or (B_ICU is False)):
+			# Bouncing for H
+			summ_entering_h = np.sum(self.flow_H)
+			summ_staying_h = np.sum((1-self.lambda_H_R-self.lambda_H_D)*self.state[:,cont.index("H")])
+
+			overflow_h = summ_entering_h - self.beds + summ_staying_h if summ_entering_h - self.beds + summ_staying_h>0 else 0
+			summ_entering_h = summ_entering_h if summ_entering_h!=0 else 1e-6
+			
+			self.B_H = self.flow_H*overflow_h/summ_entering_h
+
+			# Bouncing for ICU
+			summ_entering_icu = np.sum(self.flow_ICU)
+			summ_staying_icu = np.sum((1-self.lambda_ICU_R-self.lambda_ICU_D)*self.state[:,cont.index("ICU")])
+
+			overflow_icu = summ_entering_icu - self.icus + summ_staying_icu if summ_entering_icu - self.icus + summ_staying_icu>0 else 0
+			summ_entering_icu = summ_entering_icu if summ_entering_icu!=0 else 1e-6
+			
+			self.B_ICU = self.flow_ICU*overflow_icu/summ_entering_icu
+
+
+		# Create a population variable, this is useful to avoid division by zero
+		self.pop = np.copy(self.state[:,cont.index("N")])
+		self.pop[self.pop == 0.0]
+
+
+		# Update states
+		self.update_N()
+		self.update_S()
+		self.update_E()
+		self.update_I()
+		self.update_R()
+		self.update_Ia()
+		self.update_Ips()
+		self.update_Ims()
+		self.update_Iss()
+		self.update_Rq()
+		self.update_H()
+		self.update_ICU()
+		self.update_D()
+
+		# # Get new economic value
+		# self.economic_value = self.get_economic_value()
+		# self.deaths = self.get_deaths()
+		# self.reward = self.get_reward()
+
+		return self.new_state
+
+		# return {
+		# 	"new_state": self.new_state,
+		# 	"economic_value": self.economic_value,
+		# 	"deaths": self.deaths,
+		# 	"reward": self.reward,
+		# }
+
+	def update_contact_matrix(self):
+		for g1 in range(len(age_groups)):
+			for g2 in range(len(age_groups)):
+				self.contact_matrix[g1,g2] = 0
+				for act in range(len(activities)):
+					if self.mixing_method['name'] == "maxmin":
+						self.contact_matrix[g1,g2] += self.M[g1,g2,act]*(
+								(self.alphas[g1,act]*math.exp(self.alphas[g1,act]*self.mixing_method['param']) + self.alphas[g2,act]*math.exp(self.alphas[g2,act]*self.mixing_method['param']))
+								/(math.exp(self.alphas[g1,act]*self.mixing_method['param'])+math.exp(self.alphas[g2,act]*self.mixing_method['param']))
+							)
+					elif self.mixing_method['name'] == "mult":
+						self.contact_matrix[g1,g2] += self.M[g1,g2,act]*(self.alphas[g1,act]**self.mixing_method['param_alpha'])*(self.alphas[g2,act]**self.mixing_method['param_beta'])
+					else:
+						assert(False)
+
+	def get_flow_H(self):
+		denom = self.p_ICU + self.p_H
+		denom[denom == 0] = 1e-6
+		return self.mu*self.p_H*(self.state[:,cont.index("I")] + self.state[:,cont.index("Iss")]/denom)
+
+	def get_flow_ICU(self):
+		denom = self.p_ICU + self.p_H
+		denom[denom == 0] = 1e-6
+		return self.mu*self.p_ICU*(self.state[:,cont.index("I")] + self.state[:,cont.index("Iss")]/denom)
+
+	def update_N(self):
+		self.new_state[:,cont.index("N")] = self.state[:,cont.index("N")]+(
+			- self.m_tests*self.state[:,cont.index("I")]/self.pop
+			- self.a_tests*self.state[:,cont.index("R")]/self.pop
+			- self.mu*(self.p_H + self.p_ICU)*self.state[:,cont.index("I")]
+		)*self.dt
+
+	def update_S(self):
+		self.new_state[:,cont.index("S")] = self.state[:,cont.index("S")]+(
+			- self.beta*self.total_contacts
+		)*self.dt
+
+	def update_E(self):
+		self.new_state[:,cont.index("E")] = self.state[:,cont.index("E")]+(
+			self.beta*self.total_contacts - self.sigma*self.state[:,cont.index("E")]
+		)*self.dt
+
+	def update_I(self):
+		self.new_state[:,cont.index("I")] = self.state[:,cont.index("I")]+(
+			self.sigma*self.state[:,cont.index("E")] - self.mu*self.state[:,cont.index("I")] - self.m_tests*self.state[:,cont.index("I")]/self.pop
+		)*self.dt
+
+	def update_R(self):
+		self.new_state[:,cont.index("R")] = self.state[:,cont.index("R")]+(
+			self.mu*(1-self.p_H-self.p_ICU)*self.state[:,cont.index("I")] - self.a_tests*self.state[:,cont.index("R")]/self.pop
+		)*self.dt
+
+	def update_Ia(self):
+		self.new_state[:,cont.index("Ia")] = self.state[:,cont.index("Ia")]+(
+			self.p_Ia*self.m_tests*self.state[:,cont.index("I")]/self.pop - self.mu*self.state[:,cont.index("Ia")]
+		)*self.dt
+
+	def update_Ips(self):
+		self.new_state[:,cont.index("Ips")] = self.state[:,cont.index("Ips")]+(
+			self.p_Ips*self.m_tests*self.state[:,cont.index("I")]/self.pop - self.mu*self.state[:,cont.index("Ips")]
+		)*self.dt
+
+	def update_Ims(self):
+		self.new_state[:,cont.index("Ims")] = self.state[:,cont.index("Ims")]+(
+			self.p_Ims*self.m_tests*self.state[:,cont.index("I")]/self.pop - self.mu*self.state[:,cont.index("Ims")]
+		)*self.dt
+
+	def update_Iss(self):
+		self.new_state[:,cont.index("Iss")] = self.state[:,cont.index("Iss")]+(
+			self.p_Iss*self.m_tests*self.state[:,cont.index("I")]/self.pop - self.mu*self.state[:,cont.index("Iss")]
+		)*self.dt
+
+	def update_Rq(self):
+		self.new_state[:,cont.index("Rq")] = self.state[:,cont.index("Rq")]+(
+			self.mu*(self.state[:,cont.index("Ia")] + self.state[:,cont.index("Ips")] + self.state[:,cont.index("Ims")]) +
+			self.lambda_H_R*self.state[:,cont.index("H")] + 
+			self.lambda_ICU_R*self.state[:,cont.index("ICU")] +
+			self.a_tests*self.state[:,cont.index("R")]/self.pop
+		)*self.dt
+
+	def update_H(self):
+		tol = 1e-7
+		self.new_state[:,cont.index("H")] = self.state[:,cont.index("H")]+(
+			-(self.lambda_H_R + self.lambda_H_D)*self.state[:,cont.index("H")]
+			+(1-tol)*self.flow_H
+			-self.B_H
+		)*self.dt
+
+	def update_ICU(self):
+		tol = 1e-7
+		self.new_state[:,cont.index("ICU")] = self.state[:,cont.index("ICU")]+(
+			-(self.lambda_ICU_R + self.lambda_ICU_D)*self.state[:,cont.index("ICU")]
+			+(1-tol)*self.flow_ICU
+			-self.B_ICU
+		)*self.dt
+
+
+	def update_D(self):
+		self.new_state[:,cont.index("D")] = self.state[:,cont.index("D")]+(
+			self.lambda_H_D*self.state[:,cont.index("H")]
+			+ self.lambda_ICU_D*self.state[:,cont.index("ICU")]
+			+ self.B_H
+			+ self.B_ICU
+		)*self.dt
+
+
+
+	# # Given a state and set of alphas, returns the economic value
+	# def get_economic_value(self, state, alphas):
+	# 	value = 0
+	# 	for group in state:
+	# 		value = value + (
+	# 			self.groups[group].economics['work_value']*(
+	# 				alphas[group]['work']+
+	# 				self.groups[group].economics['lockdown_fraction']*(1-alphas[group]['work'])
+	# 			)*
+	# 			(state[group]["S"] + state[group]["E"] + state[group]["R"])
+	# 			* self.dt
+	# 		)
+	# 		# Liberate people in Rq group
+	# 		value = value + state[group]["Rq"]*self.groups[group].economics['work_value']* self.dt
+	# 	return value
+
+
+
+class SEIR_group:
+	# Time step
+	def __init__(self, group_parameters, group_initialization, dt, mixing_method, parent):
+		# Group name
+		self.name = group_parameters['name']
+		self.parameters = group_parameters['parameters']
+		self.contacts = group_parameters['contacts']
+		self.economics = group_parameters['economics']
+		self.initial_conditions = group_initialization
+		self.mixing_method = mixing_method
+		self.parent = parent
+
+		# Time step
+		self.dt = dt
+
+
---
+++ fast_gradient/test_fast.py
@@ -0,0 +1,209 @@
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir) 
+
+
+import math
+import yaml
+from group import *
+from numpy import random
+from fast_group import FastDynamicalModel
+import time
+import cProfile
+
+
+def buildAlphaDict(u_hat_array):
+    """Given an array u_hat_array, builds a dictionary for all decisions that is compatible with DynModel"""
+    u_hat_dict = {}
+    alphas = {}
+    for ag in range(0, num_age_groups):
+        u_hat_dict[age_groups[ag]] = {}
+        alphas[age_groups[ag]] = {}
+        u_hat_dict[age_groups[ag]]['Nmtest_g'] = u_hat_array[ag * num_controls + controls.index('Nmtest_g')]
+        u_hat_dict[age_groups[ag]]['Natest_g'] = u_hat_array[ag * num_controls + controls.index('Natest_g')]
+        u_hat_dict[age_groups[ag]]['BounceH_g'] = u_hat_array[ag * num_controls + controls.index('BounceH_g')] if u_hat_array[ag * num_controls + controls.index('BounceH_g')] != -1 else False
+        u_hat_dict[age_groups[ag]]['BounceICU_g'] = u_hat_array[ag * num_controls + controls.index('BounceICU_g')] if u_hat_array[ag * num_controls + controls.index('BounceICU_g')] != -1 else False
+
+        alphas[age_groups[ag]]['home'] = u_hat_array[ag * num_controls + controls.index('home')]
+        alphas[age_groups[ag]]['leisure'] = u_hat_array[ag * num_controls + controls.index('leisure')]
+        alphas[age_groups[ag]]['other'] = u_hat_array[ag * num_controls + controls.index('other')]
+        alphas[age_groups[ag]]['school'] = u_hat_array[ag * num_controls + controls.index('school')]
+        alphas[age_groups[ag]]['transport'] = u_hat_array[ag * num_controls + controls.index('transport')]
+        alphas[age_groups[ag]]['work'] = u_hat_array[ag * num_controls + controls.index('work')]
+
+    return u_hat_dict, alphas
+
+age_groups = ['age_group_0_9', 'age_group_10_19', 'age_group_20_29','age_group_30_39', 'age_group_40_49', 'age_group_50_59', 'age_group_60_69', 'age_group_70_79', 'age_group_80_plus']
+cont = [ 'S', 'E', 'I', 'R', 'N', 'Ia', 'Ips', \
+       'Ims', 'Iss', 'Rq', 'H', 'ICU', 'D' ]
+activities = ['home','leisure','other','school','transport','work']
+controls = [ 'Nmtest_g', 'Natest_g', 'BounceH_g', 'BounceICU_g' ]
+controls.extend(activities)
+
+num_age_groups = len(age_groups)
+num_controls = len(controls)
+num_activities = len(activities)
+
+simulation_params = {
+        'dt':1.0,
+        'days': 182,
+        'region': "Ile-de-France",
+        'quar_freq': 182,
+}
+
+
+# Define time variables
+simulation_params['time_periods'] = int(math.ceil(simulation_params["days"]/simulation_params["dt"]))
+
+# Define mixing method
+mixing_method = {
+    "name":"mult",
+    "param_alpha":1.0,
+    "param_beta":0.5,
+    "param":1.0
+    #"param":float(args.mixing_param) if args.mixing_param else 0.0,
+}
+
+# Read group parameters
+with open("../parameters/"+simulation_params["region"]+".yaml") as file:
+    # The FullLoader parameter handles the conversion from YAML
+    # scalar values to Python the dictionary format
+    universe_params = yaml.load(file, Loader=yaml.FullLoader)
+
+# Read initialization
+with open("../initialization/initialization.yaml") as file:
+    # The FullLoader parameter handles the conversion from YAML
+    # scalar values to Python the dictionary format
+    initialization = yaml.load(file, Loader=yaml.FullLoader)
+
+# Define policy
+with open('../benchmarks/static_infected_10.yaml') as file:
+    # The FullLoader parameter handles the conversion from YAML
+    # scalar values to Python the dictionary format
+    policy_file = yaml.load(file, Loader=yaml.FullLoader)
+alphas_vec = policy_file['alphas_vec']
+
+# Percentage infected at time 0
+perc_infected = 10
+
+
+
+
+
+# Initialize groups
+random.seed(0)
+for group in initialization:
+	for chamber in initialization[group]:
+		if chamber != "N":
+			initialization[group][chamber] = random.randint(1000)
+	initialization[group]["N"] = initialization[group]["S"] + initialization[group]["E"] + initialization[group]["I"] + initialization[group]["R"]
+
+# Randomly assign alphas_vec
+for group in alphas_vec[0]:
+	for activity in alphas_vec[0][group]:
+		alphas_vec[0][group][activity] = random.uniform(0,1)
+
+
+u = random.randint(1000, size = (num_controls * num_age_groups))
+u_hat_dict, alphas = buildAlphaDict(u)
+
+m_tests = {}
+a_tests = {}
+for g in age_groups:
+    m_tests[g] = u_hat_dict[g]['Nmtest_g']
+    a_tests[g] = u_hat_dict[g]['Natest_g']
+
+# Construct model
+dynModel = DynamicalModel(universe_params, initialization, simulation_params['dt'], simulation_params['time_periods'], mixing_method)
+initial_state = dynModel.get_state(0)
+
+# Run model for one step
+for i in range(1):
+	dynModel.take_time_step(m_tests, a_tests, alphas_vec[0])
+final_X = dynModel.get_state(1)
+
+# Convert alphas and testing and state to matrices
+def state_to_matrix(state):
+	m = np.zeros((len(age_groups),len(cont)))
+	for i in range(len(age_groups)):
+		for c in range(len(cont)):
+			m[i,c] = state[age_groups[i]][cont[c]]
+	return m
+
+def tests_to_vector(tests):
+	v = np.zeros(len(age_groups))
+	for i in range(len(age_groups)):
+		v[i] = tests[age_groups[i]]
+	return v
+
+def alphas_to_matrix(alphas):
+	m = np.zeros((len(age_groups),len(activities)))
+	for i in range(len(age_groups)):
+		for a in range(len(activities)):
+			m[i,a] = alphas[age_groups[i]][activities[a]]
+	return m
+
+def state_to_matrix(state_dict):
+	m = np.zeros((len(age_groups), len(cont)))
+	for i in range(len(age_groups)):
+		for j in range(len(cont)):
+			m[i,j] = state_dict[age_groups[i]][cont[j]]
+	return m
+
+state_matrix = state_to_matrix(initial_state)
+m_tests_vec = tests_to_vector(m_tests)
+a_tests_vec = tests_to_vector(a_tests)
+alphas_matrix = alphas_to_matrix(alphas_vec[0])
+
+fast = FastDynamicalModel(universe_params, initialization, simulation_params['dt'], mixing_method)
+new_state = fast.take_time_step(state_matrix, m_tests_vec, a_tests_vec, alphas_matrix)
+
+
+slow_total_contacts = np.zeros(len(age_groups))
+for i,group in enumerate(age_groups):
+	slow_total_contacts[i] = dynModel.groups[group].total_contacts[0]
+
+assert(np.linalg.norm(fast.total_contacts-slow_total_contacts)<1e-9)
+
+print(new_state-state_to_matrix(final_X))
+
+
+### Now we time the models
+
+# Run model for many steps
+def main1():
+	dynModel = DynamicalModel(universe_params, initialization, simulation_params['dt'], simulation_params['time_periods'], mixing_method)
+	initial_state = dynModel.get_state(0)
+
+	t0 = time.time()
+	for i in range(200):
+		dynModel.take_time_step(m_tests, a_tests, alphas_vec[0])
+	final_X = dynModel.get_state(200)
+	t1 = time.time()
+	print(t1-t0)
+
+# Same thing
+
+fast = FastDynamicalModel(universe_params, initialization, simulation_params['dt'], mixing_method)
+
+def main2():
+	state = state_to_matrix(initial_state)
+
+	t0 = time.time()
+	for i in range(200):
+		state = fast.take_time_step(state, m_tests_vec, a_tests_vec, alphas_matrix)
+	t1 = time.time()
+	print(t1-t0)
+
+cProfile.run('main1()')
+cProfile.run('main2()')
+
+
+
+
+
+
+
+
+
