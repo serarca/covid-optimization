6d81130307048f7054208c4ae7a9429c309abe89
serarca <serarcago@gmail.com>
2020-08-12 15:24:36 -0500

    changed fitting

--- benchmarks/simple_benchmarks.py
+++ benchmarks/simple_benchmarks.py
@@ -196,8 +196,9 @@ with open("../parameters/fitted.yaml") as file:
     universe_params = yaml.load(file, Loader=yaml.FullLoader)
 
 # Read initialization
-with open("../initialization/61days.yaml") as file:
+with open("../initialization/60days.yaml") as file:
 	initialization = yaml.load(file, Loader=yaml.FullLoader)
+	start_day = 60
 
 # Read econ parameters
 with open("../parameters/econ.yaml") as file:
@@ -206,210 +207,270 @@ with open("../parameters/econ.yaml") as file:
 # Define mixing parameter
 mixing_method = universe_params["mixing"]
 
+# Load gov policy
+with open("../policies/fitted.yaml") as file:
+	gov_policy = yaml.load(file, Loader=yaml.FullLoader)
+for i,p in enumerate(gov_policy):
+	if p["days_from_lockdown"] == 0:
+		start_lockdown = i
+		break
+
 
 # Parameters to try
 params_to_try = {
 	"delta_schooling":[0.5],
-	"xi":[0, 30 * 37199.03, 60 * 37199.03],
-	"icus":[2000],
+	"xi":[0, 1e6],
+	"icus":[3000],
 	"tests":[0,30000],
-	"testing":["homogeneous"]+age_groups
+	"testing":["homogeneous"]
 }
 results = []
 
-
-# We start by benchmarking the policy implemented by the government
-# Read econ parameters
-with open("../policies/fitted.yaml") as file:
-	gov_policy = yaml.load(file, Loader=yaml.FullLoader)
-
-alphas_vec = []
-start_lockdown_day = [i for i,d in enumerate(gov_policy) if d['days_from_lockdown']==0][0]
-
-
-for t in range(simulation_params['time_periods']):
-	index = t+start_lockdown_day
-	if t+start_lockdown_day >= len(gov_policy):
-		alphas_vec.append({ag:gov_policy[-1] for ag in age_groups})
-	else:
-		del gov_policy[t+start_lockdown_day]['date']
-		del gov_policy[t+start_lockdown_day]['days_from_lockdown']
-		alphas_vec.append({ag:gov_policy[t+start_lockdown_day] for ag in age_groups})
+experiment_params = {
+	"delta_schooling":0.5,
+	"xi":0,
+	"icus":3000,
+	"tests":0,
+	"testing":"homogeneous",
+}
 
 
-for delta in params_to_try["delta_schooling"]:
-	for xi in params_to_try["xi"]:
-		for icus in params_to_try["icus"]:
-			for tests in params_to_try["tests"]:
-				for testing in params_to_try["testing"]:
-					experiment_params = {
-						'delta_schooling':delta,
-						'xi':xi,
-						'icus':icus,
-					}
-					# Create dynamical method
-					dynModel = DynamicalModel(universe_params, econ_params, experiment_params, initialization, simulation_params['dt'], simulation_params['time_periods'], mixing_method)
-					if testing == "homogeneous":
-						m_tests = {ag:tests/len(age_groups) for ag in age_groups}
-						a_tests = {ag:tests/len(age_groups) for ag in age_groups}
-					elif testing in age_groups:
-						m_tests = {ag:tests if ag==testing else 0 for ag in age_groups}
-						a_tests = {ag:tests if ag==testing else 0 for ag in age_groups}
-
-					for t in range(simulation_params['time_periods']):
-						dynModel.take_time_step(m_tests, a_tests, alphas_vec[t])
-
-					results.append({
-						"heuristic":"real",
-						"delta_schooling":delta,
-						"xi":xi,
-						"icus":icus,
-						"tests":tests,
-						"testing":testing,
-						"economics_value":dynModel.get_total_economic_value(),
-						"deaths":dynModel.get_total_deaths(),
-						"reward":dynModel.get_total_reward(),
-					})
-					pickle.dump(dynModel,open(f"dynModel_real_benchmark_days_{simulation_params['time_periods']}_deltas={delta}_xi={xi}_icus={icus}_maxTests={tests}.p","wb"))
-
-					# plot_benchmark(dynModel, delta, xi, icus, tests, testing, simulation_params, "real")
+def run_government_policy(experiment_params):
 
 
-# Now we benchmark government full lockdown
-alphas={ag:gov_policy[start_lockdown_day] for ag in age_groups}
+	alphas_vec = []
+
+	for t in range(simulation_params['time_periods']):
+		index = t+start_day
+		if t+start_day >= len(gov_policy):
+			alphas_vec.append({ag:gov_policy[-1] for ag in age_groups})
+		else:
+			del gov_policy[t+start_day]['date']
+			del gov_policy[t+start_day]['days_from_lockdown']
+			alphas_vec.append({ag:gov_policy[t+start_day] for ag in age_groups})
+
+	# Create dynamical method
+	dynModel = DynamicalModel(universe_params, econ_params, experiment_params, initialization, simulation_params['dt'], simulation_params['time_periods'], mixing_method, start_day)
+	if experiment_params["testing"] == "homogeneous":
+		m_tests = {ag:experiment_params["tests"]/len(age_groups) for ag in age_groups}
+		a_tests = {ag:experiment_params["tests"]/len(age_groups) for ag in age_groups}
+
+	for t in range(simulation_params['time_periods']):
+		dynModel.take_time_step(m_tests, a_tests, alphas_vec[t])
+
+	result = {
+		"heuristic":"real",
+		"delta_schooling":experiment_params["delta_schooling"],
+		"xi":experiment_params["xi"],
+		"icus":experiment_params["icus"],
+		"tests":experiment_params["tests"],
+		"testing":experiment_params["testing"],
+		"economics_value":dynModel.get_total_economic_value(),
+		"deaths":dynModel.get_total_deaths(),
+		"reward":dynModel.get_total_reward(),	
+	}
+
+	return result
+
+def run_constant_policy(experiment_params, alpha):
+
+	# Create dynamical method
+	dynModel = DynamicalModel(universe_params, econ_params, experiment_params, initialization, simulation_params['dt'], simulation_params['time_periods'], mixing_method, start_day)
+	if experiment_params["testing"] == "homogeneous":
+		m_tests = {ag:experiment_params["tests"]/len(age_groups) for ag in age_groups}
+		a_tests = {ag:experiment_params["tests"]/len(age_groups) for ag in age_groups}
+
+	for t in range(simulation_params['time_periods']):
+		dynModel.take_time_step(m_tests, a_tests, alpha)
+
+	result = {
+		"heuristic":"real",
+		"delta_schooling":experiment_params["delta_schooling"],
+		"xi":experiment_params["xi"],
+		"icus":experiment_params["icus"],
+		"tests":experiment_params["tests"],
+		"testing":experiment_params["testing"],
+		"economics_value":dynModel.get_total_economic_value(),
+		"deaths":dynModel.get_total_deaths(),
+		"reward":dynModel.get_total_reward(),	
+	}
+
+	return result
+
+def run_full_lockdown(experiment_params):
+
+	ag_alpha = gov_policy[start_lockdown]
+	del ag_alpha["days_from_lockdown"]
+	del ag_alpha["date"]
+
+	alpha = {
+		ag:ag_alpha for ag in age_groups
+	}
+
+	return run_constant_policy(experiment_params, alpha)
+
+def run_open(experiment_params):
+	ag_alpha = {
+		"home": 1.0,
+		"leisure": 1.0,
+		"other": 1.0,
+		"school": 1.0,
+		"transport": 1.0,
+		"work": 1.0
+	}
+
+	alpha = {
+		ag:ag_alpha for ag in age_groups
+	}
+
+	return run_constant_policy(experiment_params, alpha)
+
+run_government_policy(experiment_params)
+run_full_lockdown(experiment_params)
+run_open(experiment_params)
 
 
-for delta in params_to_try["delta_schooling"]:
-	for xi in params_to_try["xi"]:
-		for icus in params_to_try["icus"]:
-			for tests in params_to_try["tests"]:
-				for testing in params_to_try["testing"]:
-					experiment_params = {
-						'delta_schooling':delta,
-						'xi':xi,
-						'icus':icus,
-					}
-					# Create dynamical method
-					dynModel = DynamicalModel(universe_params, econ_params, experiment_params, initialization, simulation_params['dt'], simulation_params['time_periods'], mixing_method)
-					if testing == "homogeneous":
-						m_tests = {ag:tests/len(age_groups) for ag in age_groups}
-						a_tests = {ag:tests/len(age_groups) for ag in age_groups}
-					elif testing in age_groups:
-						m_tests = {ag:tests if ag==testing else 0 for ag in age_groups}
-						a_tests = {ag:tests if ag==testing else 0 for ag in age_groups}
 
-					for t in range(simulation_params['time_periods']):
-						dynModel.take_time_step(m_tests, a_tests, alphas)
-
-					results.append({
-						"heuristic":"government full lockdown",
-						"delta_schooling":delta,
-						"xi":xi,
-						"icus":icus,
-						"tests":tests,
-						"testing":testing,
-						"economics_value":dynModel.get_total_economic_value(),
-						"deaths":dynModel.get_total_deaths(),
-						"reward":dynModel.get_total_reward(),
-					})
-					pickle.dump(dynModel,open(f"dynModel_gov_full_lockd_benchmark_days_{simulation_params['time_periods']}_deltas={delta}_xi={xi}_icus={icus}_maxTests={tests}.p","wb"))
-
-					# plot_benchmark(dynModel, delta, xi, icus, tests, testing, simulation_params, "govm_full_lockdown")
+# # Now we benchmark government full lockdown
+# gov_lockdown_policy = gov_policy[0]
+# del gov_lockdown_policy['date']
+# del gov_lockdown_policy['days_from_lockdown']
+# del gov_lockdown_policy['lockdown']
+# alphas={ag:gov_lockdown_policy for ag in age_groups}
 
 
-# Now we benchmark zero full lockdown
-alphas={ag:{
-	"home": 1.0,
-	"leisure": 0.0,
-	"other": 0.0,
-	"school": 0.0,
-	"transport": 0.0,
-	"work": 0.0
-} for ag in age_groups}
+
+# for delta in params_to_try["delta_schooling"]:
+# 	for xi in params_to_try["xi"]:
+# 		for icus in params_to_try["icus"]:
+# 			for tests in params_to_try["tests"]:
+# 				for testing in params_to_try["testing"]:
+# 					experiment_params = {
+# 						'delta_schooling':delta,
+# 						'xi':xi,
+# 						'icus':icus,
+# 					}
+# 					# Create dynamical method
+# 					dynModel = DynamicalModel(universe_params, econ_params, experiment_params, initialization, simulation_params['dt'], simulation_params['time_periods'], mixing_method, start_day)
+# 					if testing == "homogeneous":
+# 						m_tests = {ag:tests/len(age_groups) for ag in age_groups}
+# 						a_tests = {ag:tests/len(age_groups) for ag in age_groups}
+# 					elif testing in age_groups:
+# 						m_tests = {ag:tests if ag==testing else 0 for ag in age_groups}
+# 						a_tests = {ag:tests if ag==testing else 0 for ag in age_groups}
+
+# 					for t in range(simulation_params['time_periods']):
+# 						dynModel.take_time_step(m_tests, a_tests, alphas)
+
+# 					results.append({
+# 						"heuristic":"government full lockdown",
+# 						"delta_schooling":delta,
+# 						"xi":xi,
+# 						"icus":icus,
+# 						"tests":tests,
+# 						"testing":testing,
+# 						"economics_value":dynModel.get_total_economic_value(),
+# 						"deaths":dynModel.get_total_deaths(),
+# 						"reward":dynModel.get_total_reward(),
+# 					})
+# 					pickle.dump(dynModel,open(f"dynModel_gov_full_lockd_benchmark_days_{simulation_params['time_periods']}_deltas={delta}_xi={xi}_icus={icus}_maxTests={tests}.p","wb"))
+
+# 					# plot_benchmark(dynModel, delta, xi, icus, tests, testing, simulation_params, "govm_full_lockdown")
 
 
-for delta in params_to_try["delta_schooling"]:
-	for xi in params_to_try["xi"]:
-		for icus in params_to_try["icus"]:
-			for tests in params_to_try["tests"]:
-				for testing in params_to_try["testing"]:
-					experiment_params = {
-						'delta_schooling':delta,
-						'xi':xi,
-						'icus':icus,
-					}
-					# Create dynamical method
-					dynModel = DynamicalModel(universe_params, econ_params, experiment_params, initialization, simulation_params['dt'], simulation_params['time_periods'], mixing_method)
-					if testing == "homogeneous":
-						m_tests = {ag:tests/len(age_groups) for ag in age_groups}
-						a_tests = {ag:tests/len(age_groups) for ag in age_groups}
-					elif testing in age_groups:
-						m_tests = {ag:tests if ag==testing else 0 for ag in age_groups}
-						a_tests = {ag:tests if ag==testing else 0 for ag in age_groups}
-
-					for t in range(simulation_params['time_periods']):
-						dynModel.take_time_step(m_tests, a_tests, alphas)
-
-					results.append({
-						"heuristic":"zero full lockdown",
-						"delta_schooling":delta,
-						"xi":xi,
-						"icus":icus,
-						"tests":tests,
-						"testing":testing,
-						"economics_value":dynModel.get_total_economic_value(),
-						"deaths":dynModel.get_total_deaths(),
-						"reward":dynModel.get_total_reward(),
-					})
-					pickle.dump(dynModel,open(f"dynModel_zero_full_lock_benchmark_days_{simulation_params['time_periods']}_deltas={delta}_xi={xi}_icus={icus}_maxTests={tests}.p","wb"))
-
-					# plot_benchmark(dynModel, delta, xi, icus, tests, testing, simulation_params, "zero_full_lockdown")
-
-# Now we benchmark open
-alphas={ag:{
-	"home": 1.0,
-	"leisure": 1.0,
-	"other": 1.0,
-	"school": 1.0,
-	"transport": 1.0,
-	"work": 1.0
-} for ag in age_groups}
+# # Now we benchmark zero full lockdown
+# alphas={ag:{
+# 	"home": econ_params['upper_bounds']['home'],
+# 	"leisure": 0.0,
+# 	"other": 0.0,
+# 	"school": 0.0,
+# 	"transport": 0.0,
+# 	"work": 0.0
+# } for ag in age_groups}
 
 
-for delta in params_to_try["delta_schooling"]:
-	for xi in params_to_try["xi"]:
-		for icus in params_to_try["icus"]:
-			for tests in params_to_try["tests"]:
-				for testing in params_to_try["testing"]:
-					experiment_params = {
-						'delta_schooling':delta,
-						'xi':xi,
-						'icus':icus,
-					}
-					# Create dynamical method
-					dynModel = DynamicalModel(universe_params, econ_params, experiment_params, initialization, simulation_params['dt'], simulation_params['time_periods'], mixing_method)
-					if testing == "homogeneous":
-						m_tests = {ag:tests/len(age_groups) for ag in age_groups}
-						a_tests = {ag:tests/len(age_groups) for ag in age_groups}
-					elif testing in age_groups:
-						m_tests = {ag:tests if ag==testing else 0 for ag in age_groups}
-						a_tests = {ag:tests if ag==testing else 0 for ag in age_groups}
+# for delta in params_to_try["delta_schooling"]:
+# 	for xi in params_to_try["xi"]:
+# 		for icus in params_to_try["icus"]:
+# 			for tests in params_to_try["tests"]:
+# 				for testing in params_to_try["testing"]:
+# 					experiment_params = {
+# 						'delta_schooling':delta,
+# 						'xi':xi,
+# 						'icus':icus,
+# 					}
+# 					# Create dynamical method
+# 					dynModel = DynamicalModel(universe_params, econ_params, experiment_params, initialization, simulation_params['dt'], simulation_params['time_periods'], mixing_method, start_day)
+# 					if testing == "homogeneous":
+# 						m_tests = {ag:tests/len(age_groups) for ag in age_groups}
+# 						a_tests = {ag:tests/len(age_groups) for ag in age_groups}
+# 					elif testing in age_groups:
+# 						m_tests = {ag:tests if ag==testing else 0 for ag in age_groups}
+# 						a_tests = {ag:tests if ag==testing else 0 for ag in age_groups}
 
-					for t in range(simulation_params['time_periods']):
-						dynModel.take_time_step(m_tests, a_tests, alphas)
+# 					for t in range(simulation_params['time_periods']):
+# 						dynModel.take_time_step(m_tests, a_tests, alphas)
 
-					results.append({
-						"heuristic":"open",
-						"delta_schooling":delta,
-						"xi":xi,
-						"icus":icus,
-						"tests":tests,
-						"testing":testing,
-						"economics_value":dynModel.get_total_economic_value(),
-						"deaths":dynModel.get_total_deaths(),
-						"reward":dynModel.get_total_reward(),
-					})
-					pickle.dump(dynModel,open(f"dynModel_open_benchmark_days_{simulation_params['time_periods']}_deltas={delta}_xi={xi}_icus={icus}_maxTests={tests}.p","wb"))
-					# plot_benchmark(dynModel, delta, xi, icus, tests, testing, simulation_params, "open")
+# 					results.append({
+# 						"heuristic":"zero full lockdown",
+# 						"delta_schooling":delta,
+# 						"xi":xi,
+# 						"icus":icus,
+# 						"tests":tests,
+# 						"testing":testing,
+# 						"economics_value":dynModel.get_total_economic_value(),
+# 						"deaths":dynModel.get_total_deaths(),
+# 						"reward":dynModel.get_total_reward(),
+# 					})
+# 					pickle.dump(dynModel,open(f"dynModel_zero_full_lock_benchmark_days_{simulation_params['time_periods']}_deltas={delta}_xi={xi}_icus={icus}_maxTests={tests}.p","wb"))
 
-pd.DataFrame(results).to_excel(f"simulations-{simulation_params['days']}-days.xlsx")
+# 					# plot_benchmark(dynModel, delta, xi, icus, tests, testing, simulation_params, "zero_full_lockdown")
+
+# # Now we benchmark open
+# alphas={ag:{
+# 	"home": econ_params['upper_bounds']['home'],
+# 	"leisure": econ_params['upper_bounds']['leisure'],
+# 	"other": econ_params['upper_bounds']['other'],
+# 	"school": econ_params['upper_bounds']['school'],
+# 	"transport": econ_params['upper_bounds']['transport'],
+# 	"work": econ_params['upper_bounds']['work']
+# } for ag in age_groups}
+
+
+# for delta in params_to_try["delta_schooling"]:
+# 	for xi in params_to_try["xi"]:
+# 		for icus in params_to_try["icus"]:
+# 			for tests in params_to_try["tests"]:
+# 				for testing in params_to_try["testing"]:
+# 					experiment_params = {
+# 						'delta_schooling':delta,
+# 						'xi':xi,
+# 						'icus':icus,
+# 					}
+# 					# Create dynamical method
+# 					dynModel = DynamicalModel(universe_params, econ_params, experiment_params, initialization, simulation_params['dt'], simulation_params['time_periods'], mixing_method, start_day)
+# 					if testing == "homogeneous":
+# 						m_tests = {ag:tests/len(age_groups) for ag in age_groups}
+# 						a_tests = {ag:tests/len(age_groups) for ag in age_groups}
+# 					elif testing in age_groups:
+# 						m_tests = {ag:tests if ag==testing else 0 for ag in age_groups}
+# 						a_tests = {ag:tests if ag==testing else 0 for ag in age_groups}
+
+# 					for t in range(simulation_params['time_periods']):
+# 						dynModel.take_time_step(m_tests, a_tests, alphas)
+
+# 					results.append({
+# 						"heuristic":"open",
+# 						"delta_schooling":delta,
+# 						"xi":xi,
+# 						"icus":icus,
+# 						"tests":tests,
+# 						"testing":testing,
+# 						"economics_value":dynModel.get_total_economic_value(),
+# 						"deaths":dynModel.get_total_deaths(),
+# 						"reward":dynModel.get_total_reward(),
+# 					})
+# 					pickle.dump(dynModel,open(f"dynModel_open_benchmark_days_{simulation_params['time_periods']}_deltas={delta}_xi={xi}_icus={icus}_maxTests={tests}.p","wb"))
+# 					# plot_benchmark(dynModel, delta, xi, icus, tests, testing, simulation_params, "open")
+
+# pd.DataFrame(results).to_excel(f"simulations-{simulation_params['days']}-days.xlsx")
