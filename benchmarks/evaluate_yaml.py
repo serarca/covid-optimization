b3b75e41ab292479d4ca2279cef1911d437bd533
Xavier Sebastian Warnes <xwarnes@yen2.stanford.edu>
2020-09-25 08:19:57 -0700

    added all groups results

--- benchmarks/evaluate_yaml.py
+++ benchmarks/evaluate_yaml.py
@@ -208,9 +208,9 @@ def plot_benchmark(dynModel, result):
 
 
 
-#heuristics = ["real","full_open","full_lockdown","constant_gradient","time_gradient","age_group_gradient","dynamic_gradient","linearization_heuristic", "linearization_heuristic_Prop_Bouncing"]
-#heuristics = ["real","full_open","full_lockdown","constant_gradient","time_gradient","age_group_gradient","dynamic_gradient"]
-heuristics = ["real","full_open","full_lockdown","constant_gradient","time_gradient"]
+heuristics = ["dynamic_gradient","linearization_heuristic", "linearization_heuristic_Prop_Bouncing"]
+# heuristics = ["real","full_open","full_lockdown","constant_gradient","time_gradient","age_group_gradient","dynamic_gradient"]
+# heuristics = ["real","full_open","full_lockdown","constant_gradient","time_gradient"]
 #heuristics = ["dynamic_gradient"]
 groups = "all"
 
@@ -306,5 +306,5 @@ for h in heuristics:
 			
 			all_data.append(data)
 
-pd.DataFrame(all_data).to_excel("results/results-groups={groups}.xlsx")
+pd.DataFrame(all_data).to_excel(f"results/results-groups={groups}.xlsx")
 
