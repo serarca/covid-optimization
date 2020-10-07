261e27c6e29eb660a30cbeee692c7ce5de1d6e80
serarca <serarcago@gmail.com>
2020-10-06 17:22:08 -0700

    added jobs

--- benchmarks/build_bash_script.py
+++ benchmarks/build_bash_script.py
@@ -76,3 +76,44 @@ for delta in params_to_try["delta_schooling"]:
 						    	)
 						    )
 
+# Now write the sherlock scripts
+
+counter = 0
+open('sherlock_master.sh', 'w').close()
+for delta in params_to_try["delta_schooling"]:
+	for xi in params_to_try["xi"]:
+		for icus in params_to_try["icus"]:
+			for tests in params_to_try["tests"]:
+				for testing in params_to_try["testing"]:
+					for eta in params_to_try["eta"]:
+						open('sherlock_scripts/script_%d.sh'%counter, 'w').close()
+						with open('sherlock_scripts/script_%d.sh'%counter, 'a') as the_file:
+							the_file.write(
+						 		"""#!/bin/bash
+#
+#SBATCH --job-name=test
+#
+#SBATCH --time=30:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=1G
+
+ml python/3.6.1
+python3 simple_benchmarks.py --delta %f --icus %d --eta %f --groups all --xi %f --a_tests %d --m_tests %d
+python3 constant_gradient_benchmarks.py --delta %f --icus %d --eta %f --groups all --xi %f --a_tests %d --m_tests %d
+python3 constant_gradient_benchmarks.py --delta %f --icus %d --eta %f --groups all --xi %f --a_tests %d --m_tests %d
+"""%(
+									delta, icus, eta, xi, tests[1], tests[0],
+									delta, icus, eta, xi, tests[1], tests[0],
+									delta, icus, eta, xi, tests[1], tests[0]
+								)
+							)
+						with open('sherlock_master.sh', 'a') as the_file:
+							the_file.write("source sherlock_scripts/script_%d.sh\n"%counter)
+						counter+=1
+
+
+
+
+
+
---
+++ benchmarks/sherlock_scripts/script_513.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#SBATCH --job-name=test
+#
+#SBATCH --time=30:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=1G
+
+ml python/3.6.1
+python3 simple_benchmarks.py --delta 1.000000 --icus 2600 --eta 0.000000 --groups all --xi 929975.750000 --a_tests 120000 --m_tests 120000
+python3 constant_gradient_benchmarks.py --delta 1.000000 --icus 2600 --eta 0.000000 --groups all --xi 929975.750000 --a_tests 120000 --m_tests 120000
+python3 constant_gradient_benchmarks.py --delta 1.000000 --icus 2600 --eta 0.000000 --groups all --xi 929975.750000 --a_tests 120000 --m_tests 120000
