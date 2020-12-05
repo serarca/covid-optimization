9987196d98a819b971c9036d647d37e097caef9a
serarca <serarcago@gmail.com>
2020-12-04 20:16:51 -0800

    m

--- benchmarks/sherlock_scripts/script_100.sh
+++ benchmarks/sherlock_scripts/script_100.sh
@@ -8,8 +8,8 @@
 #SBATCH --mem-per-cpu=1G
 
 ml python/3.6.1
-python3 simple_benchmarks.py --delta 0.500000 --icus 2900 --eta 0.100000 --groups all --xi 371990.300000 --a_tests 0 --m_tests 30000
-python3 time_gradient_benchmarks.py --delta 0.500000 --icus 2900 --eta 0.100000 --groups all --xi 371990.300000 --a_tests 0 --m_tests 30000
-python3 age_group_gradient_benchmarks.py --delta 0.500000 --icus 2900 --eta 0.100000 --groups all --xi 371990.300000 --a_tests 0 --m_tests 30000
-python3 activity_gradient_benchmarks.py --delta 0.500000 --icus 2900 --eta 0.100000 --groups all --xi 371990.300000 --a_tests 0 --m_tests 30000
-python3 dynamic_gradient_benchmarks.py --delta 0.500000 --icus 2900 --eta 0.100000 --groups all --xi 371990.300000 --a_tests 0 --m_tests 30000
+python3 simple_benchmarks.py --delta 1.000000 --icus 2500 --eta 0.100000 --groups all --xi 5579854.500000 --a_tests 0 --m_tests 0
+python3 time_gradient_benchmarks.py --delta 1.000000 --icus 2500 --eta 0.100000 --groups all --xi 5579854.500000 --a_tests 0 --m_tests 0
+python3 age_group_gradient_benchmarks.py --delta 1.000000 --icus 2500 --eta 0.100000 --groups all --xi 5579854.500000 --a_tests 0 --m_tests 0
+python3 activity_gradient_benchmarks.py --delta 1.000000 --icus 2500 --eta 0.100000 --groups all --xi 5579854.500000 --a_tests 0 --m_tests 0
+python3 dynamic_gradient_benchmarks.py --delta 1.000000 --icus 2500 --eta 0.100000 --groups all --xi 5579854.500000 --a_tests 0 --m_tests 0
