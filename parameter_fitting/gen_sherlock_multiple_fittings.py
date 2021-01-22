650090bb33f35ccfac50a3fcfa7d85b1c0857a23
serarca <serarcago@gmail.com>
2021-01-22 11:03:33 -0500

    m

--- parameter_fitting/gen_sherlock_multiple_fittings.py
+++ parameter_fitting/gen_sherlock_multiple_fittings.py
@@ -46,6 +46,7 @@ for identifier in range(14):
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=1G
 
+ml python/3.6.1
 python3 ProcessMultipleFittings.py --identifier %d
 """%(identifier)
 		)
--- parameter_fitting/sherlock_multiple_fittings/script_0.sh
+++ parameter_fitting/sherlock_multiple_fittings/script_0.sh
@@ -7,4 +7,5 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=1G
 
+ml python/3.6.1
 python3 ProcessMultipleFittings.py --identifier 0
